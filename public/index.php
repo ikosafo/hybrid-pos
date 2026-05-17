72ccf79659146b579232657d8114ba69d788ea98
ikosafo <ikosafo@gmail.com>
2026-05-17 14:41:24 +0000

    New updates

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 // ── Parse URI ─────────────────────────────────
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = preg_replace('#^/public#', '', $uri);
